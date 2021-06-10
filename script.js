4524fef501fbe0039010bcace2f7da7e8f011754
Alena <elenila_skinef@mail.ru>
2021-06-10 18:21:55 +0300

    added toggle theme

---
+++ script.js
@@ -0,0 +1,10 @@
+//toggle theme 
+function toggleTheme(theme) {
+    const body = document.body;
+    body.classList.remove('dark', 'light')
+    if (theme == 'dark') {
+        body.classList.add('dark')
+    } else {
+        body.classList.add('light')
+    }
+}
