f8ee2a7fc89b7585dc49f6a73137b21891cba50d
Alena <elenila_skinef@mail.ru>
2021-08-16 23:56:21 +0300

    added list Ideas

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ let inputWish = document.querySelector('#input-wish');
 let wishList = [
     // .. wish objects
 ];
-
+//Ideas
+let ideasList = document.querySelector('#ideas-list');
+let inputIdea = document.querySelector('#input-idea');
+let ideas = [
+    // .. ideas objects
+];
 
 //To-Do List
 function loadLocalStorage() {
@@ -47,7 +52,7 @@ function showTasks() {
 
     toDoList.forEach(function(item, index) {
         // Clear input task
-        document.getElementById("input-task").value = '';
+        inputTask.value = '';
 
         tasksTemplate += `
         <li>
@@ -71,7 +76,7 @@ function removeTask(index) {
     updateLocalStorage();
     showTasks();
 
-    // Remove  a task from from list
+    // Remove  a task from list
     return this.parentNode.remove();
 }
 
@@ -118,7 +123,7 @@ function showWishes() {
 
     wishList.forEach(function(item, index) {
         // Clear input wish
-        document.getElementById("input-wish").value = '';
+        inputWish.value = '';
 
         wishesTemplate += `
         <li>
@@ -153,10 +158,73 @@ function checkedWish(index) {
     showWishes();
 }
 
+
 //Ideas
+function loadLocalStorageForIdeas() {
+    //We will use local storage to store the ideas. The localStorage property allows saving key/value pairs right in a web browser.
+    if (localStorage.getItem("ideas")) {
+        ideas = JSON.parse(localStorage.getItem("ideas")) || [];
+        showIdeas();
+    }
+}
 
+loadLocalStorageForIdeas();
 
+function addIdea() {
 
+    let newIdea = {
+        ideaName: inputIdea.value,
+        checked: false
+    }
+
+    if (inputIdea.value !== "") {
+        // Add idea to list and localstorage
+        ideas.push(newIdea);
+        showIdeas();
+        updateLocalStorageForIdeas();
+    }
+}
+
+//Show ideas on the screen
+function showIdeas() {
+    let ideasTemplate = '';
+
+    ideas.forEach(function(item, index) {
+        // Clear input idea
+        inputIdea.value = '';
+
+        ideasTemplate += `
+        <li>
+            <input type="checkbox" class="check" onclick="checkedIdea(${index})" ${item.checked ? 'checked' : ''}>
+            <span class="span-idea">${item.ideaName}</span>
+            <button class="delete-idea-btn"  onclick="removeIdea(${index})">Remove</button>
+        </li>
+        `;
+    });
+
+    ideasList.innerHTML = ideasTemplate;
+}
+
+function updateLocalStorageForIdeas() {
+    localStorage.setItem("ideas", JSON.stringify(ideas));
+}
+
+function removeIdea(index) {
+    // Remove  a idea from local storage
+    ideas.splice(index, 1);
+    updateLocalStorageForIdeas();
+    showIdeas();
+
+    // Remove  a idea from list
+    return this.parentNode.remove();
+}
+
+// Update state of idea and save to local storage
+function checkedIdea(index) {
+    ideas[index].checked = !ideas[index].checked;
+    updateLocalStorageForIdeas();
+    showIdeas();
+}
 
 
 function toggleTheme(theme) {
