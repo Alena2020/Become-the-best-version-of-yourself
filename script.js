6ad4a6eb118bc5c680e34f312c00aa56e85494ba
Alena <elenila_skinef@mail.ru>
2021-08-16 21:50:43 +0300

    added Wish List

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 
 
 //To-Do List
-
-let addTaskButton = document.querySelector('#add-task-button');
-let removeButtons = document.querySelectorAll('.delete-btn');
 let listTask = document.querySelector('#task-list');
 let inputTask = document.querySelector('#input-task');
 let toDoList = [
     // .. tasks objects
 ];
+//Wish List
+let wishesList = document.querySelector('#wishes-list');
+let inputWish = document.querySelector('#input-wish');
+let wishList = [
+    // .. wish objects
+];
 
+
+//To-Do List
 function loadLocalStorage() {
     //We will use local storage to store the tasks. The localStorage property allows saving key/value pairs right in a web browser.
     if (localStorage.getItem("tasks")) {
@@ -77,15 +82,78 @@ function checkedTask(index) {
     showTasks();
 }
 
-function clearAll() {
-    localStorage.clear();
-    while (listTask.firstChild) {
-        listTask.removeChild(listTask.firstChild);
+//Weekly Task Tracker
+
+
+//Wish List
+
+function loadLocalStorageForWishList() {
+    //We will use local storage to store the wishes. The localStorage property allows saving key/value pairs right in a web browser.
+    if (localStorage.getItem("wishes")) {
+        wishList = JSON.parse(localStorage.getItem("wishes")) || [];
+        showWishes();
     }
-    toDoList = [];    
 }
 
-//
+loadLocalStorageForWishList();
+
+function addWish() {
+
+    let newWish = {
+        wishName: inputWish.value,
+        checked: false
+    }
+
+    if (inputWish.value !== "") {
+        // Add wish to list and localstorage
+        wishList.push(newWish);
+        showWishes();
+        updateLocalStorageForWishList();
+    }
+}
+
+//Show wishes on the screen
+function showWishes() {
+    let wishesTemplate = '';
+
+    wishList.forEach(function(item, index) {
+        // Clear input wish
+        document.getElementById("input-wish").value = '';
+
+        wishesTemplate += `
+        <li>
+            <input type="checkbox" class="check" onclick="checkedWish(${index})" ${item.checked ? 'checked' : ''}>
+            <span class="span-wish">${item.wishName}</span>
+            <button class="delete-wish-btn"  onclick="removeWish(${index})">Remove</button>
+        </li>
+        `;
+    });
+
+    wishesList.innerHTML = wishesTemplate;
+}
+
+function updateLocalStorageForWishList() {
+    localStorage.setItem("wishes", JSON.stringify(wishList));
+}
+
+function removeWish(index) {
+    // Remove  a wish from local storage
+    wishList.splice(index, 1);
+    updateLocalStorageForWishList();
+    showWishes();
+
+    // Remove  a wish from list
+    return this.parentNode.remove();
+}
+
+// Update state of wish and save to local storage
+function checkedWish(index) {
+    wishList[index].checked = !wishList[index].checked;
+    updateLocalStorageForWishList();
+    showWishes();
+}
+
+//Ideas
 
 
 
