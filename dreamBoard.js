39b6668864b75a85984bc05b442411800e29c9cf
Alena <elenila_skinef@mail.ru>
2021-08-23 19:51:33 +0300

    added LocalStorage for Dream board

--- dreamBoard.js
+++ dreamBoard.js
@@ -1,26 +1,50 @@
 "use strict";
 
+let inputDream = 'Your dreams here...';
 let dreamList = document.querySelector('#dream-list');
-let dreamBoard = ["Friends", "Family",  "House", "Travelling", "Hobby", "Brightness of life", "Lifestyle", "Sports", "Health", "Work",  "Skills", "Finance"];
+let dreams = [
+        {dreamName: "Friends", dreamText: ''},
+        {dreamName: "Family", dreamText: ''},
+        {dreamName: "House", dreamText: ''},
+        {dreamName: "Travelling", dreamText: ''},
+        {dreamName: "Hobby", dreamText: ''},
+        {dreamName: "Brightness of life", dreamText: ''},
+        {dreamName: "Lifestyle", dreamText: ''},
+        {dreamName: "Sports", dreamText: ''},
+        {dreamName: "Health", dreamText: ''},
+        {dreamName: "Work", dreamText: ''},
+        {dreamName: "Skills", dreamText: ''},
+        {dreamName: "Finance", dreamText: ''}
+];
 
-console.log( dreamBoard);
+function loadLocalStorageForDreamBoard() {
+    //We will use local storage to store the dreams. The localStorage property allows saving key/value pairs right in a web browser.
+    if (localStorage.getItem("dreams")) {
+        dreams = JSON.parse(localStorage.getItem("dreams"));       
+        showDreamBoard();
+    }
+}
 
-function loadDreamBoard() {
+loadLocalStorageForDreamBoard();
+
+function showDreamBoard() {
   let dreamsTemplate = '';
 
-  dreamBoard.forEach(function(dream) {    
+  dreams.forEach(function(dream, index) {    
     dreamsTemplate += `
           <li class="card-dream">
-              <h4>${dream}</h4>
-              <textarea class="input-dream" placeholder="Your dreams here"></textarea>       
+              <h4>${dream.dreamName}</h4>
+              <textarea class="input-dream" placeholder="Your dreams here" id="${index}" >${dream.dreamText }</textarea>       
           </li>
         `;
 
-    dreamList.innerHTML = dreamsTemplate;
-});
-  
+    dreamList.innerHTML = dreamsTemplate;     
+  });
 }
-loadDreamBoard();
 
+showDreamBoard();
 
+function updateLocalStorageforDreamBoard() {    
+    localStorage.getItem('dreams', JSON.stringify(dreams));    
+}
 
