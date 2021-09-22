0b317555d2c2ee2eee90c314fff95eb0b806138a
Alena <elenila_skinef@mail.ru>
2021-09-22 22:56:09 +0300

    added tabs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+document.getElementById("defaultOpen").click();
+
 function toggleTheme(theme) {
     const body = document.body;
     body.classList.remove('dark', 'light')
@@ -8,4 +10,24 @@ function toggleTheme(theme) {
     } else {
         body.classList.add('light')
     }
+}
+
+function openSection(evt, sectionName) {
+    let i;
+
+  // Get all the elements using class= "section-content" and hide them
+    let sectionContent = document.getElementsByClassName("section-content");
+    for (i = 0; i < sectionContent.length; i++) {
+    sectionContent[i].style.display = "none";
+    }
+
+  // Get all the elements using class= "menu-links" and delete class "active"
+    let menuLinks = document.getElementsByClassName("menu-links");
+    for (i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].className = menuLinks[i].className.replace(" active", "");
+    }
+
+  // Show the current tab and add an "active" class for the button that opened the tab
+    document.getElementById(sectionName).style.display = "flex";
+    evt.currentTarget.className += " active";
 }
